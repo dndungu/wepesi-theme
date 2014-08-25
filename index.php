73dbd3b202a1ec74f75eb17a9bc4c120471d76e2
David Njuguna <dnjuguna@gmail.com>
2014-08-25 22:43:52 +0300

    fixed php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <main class="main">
-	<?php if(have()) : ?>
+	<?php if(have_posts()) : ?>
 	<?php while(have_posts()) : the_post();?>
 		<article <?php post_class()?>>
 			<h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
